fb7b340024203efbc9e1f0c26a611af1a45b6ee7
Enrique Berrueta <eabz@polispay.org>
2021-01-26 14:13:36 -0600

    .

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/eabz/evm-playground/vm"
+	"github.com/ethereum/evmc/v7/bindings/go/evmc"
+	"github.com/olympus-protocol/ogen/pkg/bls"
 )
+
+var senderSecret, _ = hex.DecodeString("29ca7497918b5526c355f627aa6219d2c8fd5dba93a735274e457b7489240df2")
+var sender, _ = bls.SecretKeyFromBytes(senderSecret)
 
 func main() {
 	m, err := vm.NewVM()
@@ -11,10 +17,10 @@ func main() {
 		panic(err)
 	}
 
-	ver := m.Version()
-	name := m.Name()
+	addr := evmc.Address{}
+	h := evmc.Hash{}
+	code := []byte("\x43\x60\x00\x52\x59\x60\x00\xf3")
 
-	fmt.Println(ver)
-	fmt.Println(name)
-
+	output, gasLeft, err := m.Execute(evmc.Istanbul, evmc.Call, false, 1, 999, addr, addr, code, h, nil, h)
+	fmt.Println(output, gasLeft, err)
 }
--- vm/host.go
+++ vm/host.go
@@ -1,73 +1,111 @@
 package vm
 
 import (
+	"fmt"
 	"github.com/dgraph-io/badger"
 	"github.com/eabz/evm-playground/csmt"
 	"github.com/ethereum/evmc/v7/bindings/go/evmc"
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/filter"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"sync"
 )
 
-type Host struct {
-	db csmt.Tree
+type MockState struct {
+	balances map[[20]byte]uint64
+	nonces   map[[20]byte]uint64
+	lock     sync.Mutex
 }
 
+type Host struct {
+	tree      csmt.Tree
+	db        *leveldb.DB
+	mockState *MockState
+}
+
+// AccountExists checks if a specif contract account exits withint the bytecode storage
 func (h Host) AccountExists(addr evmc.Address) bool {
-	panic("implement me AccountExists")
+	fmt.Println(addr)
+	return true
 }
 
 func (h Host) GetStorage(addr evmc.Address, key evmc.Hash) evmc.Hash {
-	panic("implement me GetStorage")
+	fmt.Println(addr, key)
+	return evmc.Hash{}
 }
 
 func (h Host) SetStorage(addr evmc.Address, key evmc.Hash, value evmc.Hash) evmc.StorageStatus {
-	panic("implement me SetStorage")
+	fmt.Println(addr, key, value)
+	return evmc.StorageAdded
 }
 
 func (h Host) GetBalance(addr evmc.Address) evmc.Hash {
-	panic("implement me GetBalance")
+	fmt.Println(addr)
+	return evmc.Hash{}
 }
 
 func (h Host) GetCodeSize(addr evmc.Address) int {
-	panic("implement me GetCodeSize")
+	fmt.Println(addr)
+	return 0
 }
 
 func (h Host) GetCodeHash(addr evmc.Address) evmc.Hash {
-	panic("implement me GetCodeHash")
+	fmt.Println(addr)
+	return evmc.Hash{}
 }
 
 func (h Host) GetCode(addr evmc.Address) []byte {
-	panic("implement me GetCode")
+	fmt.Println(addr)
+	return []byte{}
 }
 
 func (h Host) Selfdestruct(addr evmc.Address, beneficiary evmc.Address) {
-	panic("implement me Selfdestruct")
+	fmt.Println(addr, beneficiary)
+	return
 }
 
 func (h Host) GetTxContext() evmc.TxContext {
-	panic("implement me GetTxContext")
+	return evmc.TxContext{}
 }
 
 func (h Host) GetBlockHash(number int64) evmc.Hash {
-	panic("implement me GetBlockHash")
+	fmt.Println(number)
+	return evmc.Hash{}
 }
 
 func (h Host) EmitLog(addr evmc.Address, topics []evmc.Hash, data []byte) {
-	panic("implement me EmitLog")
+	fmt.Println(addr, topics, data)
 }
 
 func (h Host) Call(kind evmc.CallKind, destination evmc.Address, sender evmc.Address, value evmc.Hash, input []byte, gas int64, depth int, static bool, salt evmc.Hash) (output []byte, gasLeft int64, createAddr evmc.Address, err error) {
-	panic("implement me Call")
+	fmt.Println(kind, destination, sender, value, input, gas, depth, static, salt)
+	return nil, 0, [20]byte{}, err
 }
 
 func NewHost() (evmc.HostContext, error) {
-	opts := badger.Options{
-		Dir: "./database",
+	levelopts := &opt.Options{
+		ErrorIfExist:           false,
+		Strict:                 opt.DefaultStrict,
+		Compression:            opt.SnappyCompression,
+		Filter:                 filter.NewBloomFilter(10),
+		DisableSeeksCompaction: true,
 	}
-	db, err := badger.Open(opts)
+
+	db, err := leveldb.OpenFile("./data/bytecodes", levelopts)
 	if err != nil {
 		return nil, err
 	}
-	bdb := csmt.NewBadgerTreeDB(db)
+
+	tree, err := badger.Open(badger.DefaultOptions("./data/tree").WithLogger(nil))
+	if err != nil {
+		return nil, err
+	}
+
+	bdb := csmt.NewBadgerTreeDB(tree)
+
 	return &Host{
-		db: csmt.NewTree(bdb),
+		db:   db,
+		tree: csmt.NewTree(bdb),
 	}, nil
+
 }
